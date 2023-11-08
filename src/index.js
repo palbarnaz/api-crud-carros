870f723d369101f3c76bcca11e88a8790103787a
Paola Albarnaz <paola.silva.nh1@gmail.com>
2023-11-07 22:48:45 -0300

    feat: cria rota de criar carro e de listar carros

--- src/index.js
+++ src/index.js
@@ -2,8 +2,29 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-app.get('/turma17', (request, response) => {
-return response.json('OK Turma')
+let carros = []
+let contador = 1
+
+
+app.get('/carros', (request, response) => {
+  return response.json(carros)
 });
+
+
+app.post('/carros', (request, response) => {
+  const infoRequest = request.body
+
+  const novoCarro = {
+      id: contador++,
+      modelo: infoRequest.modelo,
+      marca: infoRequest.marca,
+      ano: infoRequest.ano,
+      cor: infoRequest.cor,
+      preco: infoRequest.preco
+  }
+  carros.push(novoCarro)
+  return response.status(201).json(novoCarro)
+});
+
 
 app.listen(8080, () => console.log("Servidor iniciado"));
