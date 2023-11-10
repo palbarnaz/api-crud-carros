68ac6095841395d63777c4142f45abafefb6dcbc
Paola Albarnaz <paola.silva.nh1@gmail.com>
2023-11-09 21:51:45 -0300

    feat: adiciona rota de filtrar um carro por id

--- src/index.js
+++ src/index.js
@@ -4,11 +4,35 @@ app.use(express.json());
 
 let carros = []
 let contador = 1
-
+     
 
 app.get('/carros', (request, response) => {
   return response.json(carros)
 });
+
+
+
+// rota utilizando query params
+app.get('/carros/pegar-id-query', (request, response) => {
+  const id = request.query
+
+  const carro = carros.filter(carro => carro.id == Number(id.id))
+
+  if(carro.length == 0){
+     return response.status(401).json('id inválido')
+  }
+  
+  return response.json(carro)
+
+})
+
+// rota utilizando route params
+app.get('/carros/:id?', (request, response) => {
+  const id = request.params
+  const carro = carros.filter(carro => carro.id == Number(id.id))
+  return response.json(carro)
+
+})
 
 
 app.post('/carros', (request, response) => {
@@ -25,6 +49,7 @@ app.post('/carros', (request, response) => {
   carros.push(novoCarro)
   return response.status(201).json(novoCarro)
 });
+
 
 
 app.listen(8080, () => console.log("Servidor iniciado"));
